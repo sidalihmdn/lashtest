feb63553119e21ef28925b51892cd2f29ccedad6
Sid-Ali Hamdane <sidali.hamdane@rakuten.com>
2026-05-12 17:31:18 +0200

    working on the dsl that will be used in xml assertions

--- src/lashtest/assertions/xml/xml_assertions.py
+++ src/lashtest/assertions/xml/xml_assertions.py
@@ -1,5 +1,13 @@
+from lxml import etree
+from .xpath_selection import XpathSelection
+
 class XmlAssertions:
+    def __init__(self, response):
+        self.response = response
+        self._xml = etree.fromstring(response.body.encode())
+
     def xpath(self, expr):
-        pass
+        return XpathSelection(expr=?, node=?)
+
 
     
---
+++ src/lashtest/assertions/xml/xpath_selection.py
@@ -0,0 +1,3 @@
+class XpathSelection:
+    def __init__(self, expr:str, nodes):
+        pass
